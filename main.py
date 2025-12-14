0c7b99bfa71f7b438d5ab7d72d7de66ae6cdcb4c
Mike Harris <jammin804@gmail.com>
2025-12-14 14:07:37 -0500

    Adding player file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 
@@ -10,12 +11,15 @@ def main():
     
     game_clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, 20)
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         game_clock.tick(60)
         dt = game_clock.get_time() / 1000.0  # Delta time in seconds
